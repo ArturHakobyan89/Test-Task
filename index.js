0978df50676b878c218c8f79b90144bbaabac1b6
ArturHakobyan89 <artur.hakobian89@gmail.com>
2023-02-04 10:38:30 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+  return cartItemsPrices.reduce((a,b) => +a + +b).toFixed(2) == WINNING_SUM
 }
 
 module.exports = doesCartWinPrize;
